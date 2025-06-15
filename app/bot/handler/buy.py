14500f8b8e84422dcfc6d814a04c528ef09ea728
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2025-06-15 16:00:01 +0000

    push

--- app/bot/handler/buy.py
+++ app/bot/handler/buy.py
@@ -14,6 +14,8 @@ load_dotenv()
 
 logger = logging.getLogger(__name__)
 
+SELECTING_OPTION, WAITING_FOR_INPUT,CONFIRM = range(3)
+
 async def get_user_service():
     # Assuming you have access to user_repository here
     user_repository = UserRepository()  # You'll need to create this
@@ -29,38 +31,59 @@ async def buy_command(update: Update,
     Handle the /balance command.
     Buy Stablecoin USDT / USDC
     """
-    user = update.effective_user
-    user_info = update.message.from_user
-    user_service = await get_user_service()
-    wallet_service = await get_wallet_service()
-    swap = JupiterSwap()
+    # user = update.effective_user
+    # user_info = update.message.from_user
+    # user_service = await get_user_service()
+    # wallet_service = await get_wallet_service()
+    # swap = JupiterSwap()
+    keyboard = [
+        [InlineKeyboardButton("USDT", callback_data="usdt")],
+        [InlineKeyboardButton("USDC", callback_data="usdc")],
+        [InlineKeyboardButton("PYUSD", callback_data="pyusd")]
+    ]
+    
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Choose an option:", reply_markup=reply_markup)
+    return SELECTING_OPTION
 
-    try:
-        user = await user_service.get_user(str(user.id))
-        if not user:
-            raise ValueError("User not Found in DB")
-        wallets = await wallet_service.get_user_wallets(str(user.id),chain=Chain.SOLANA)
-        address = wallets[0].address
-        sol_mint = swap.tokens["SOL"]
-        bal =  swap.get_token_balance(address,sol_mint)
-        if bal == 0.0 :
-            balance_message = (
-                f"""
-💰 Your current balance is insufficient for this transaction. Please deposit /fund to proceed. 🔄
+async def button_handler(update:Update,context:ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    user_id = query.from_user.id
+    selected_option = query.data
+    context.user_data["selected_option"] = selected_option
 
-Best regards,
-The Obverse Team ✨
-                    """
-            )
-            await update.message.reply_text(text=balance_message,parse_mode='markdown',disable_web_page_preview=True)
+    await query.edit_message_text(f"Please enter your {selected_option.replace('_','')}:")
+    return WAITING_FOR_INPUT
+
+    # try:
+    #     user = await user_service.get_user(str(user.id))
+    #     if not user:
+    #         raise ValueError("User not Found in DB")
+    #     wallets = await wallet_service.get_user_wallets(str(user.id),chain=Chain.SOLANA)
+    #     address = wallets[0].address
+    #     sol_mint = swap.tokens["SOL"]
+    #     bal =  swap.get_token_balance(address,sol_mint)
+
+
+#         if bal == 0.0 :
+#             balance_message = (
+#                 f"""
+# 💰 Your current balance is insufficient for this transaction. Please deposit /fund to proceed. 🔄
+
+# Best regards,
+# The Obverse Team ✨
+#                     """
+#             )
+#             await update.message.reply_text(text=balance_message,parse_mode='markdown',disable_web_page_preview=True)
 
         # await update.message.reply_text(text=balance_message,parse_mode='markdown',disable_web_page_preview=True)
-    except Exception as e:
-        logger.error(f"Error in fund command for user {user.id}: {str(e)}")
-        await update.message.reply_text(
-            "⚠️ An error occurred while processing your request. Please try again later."
-        )
-        raise
+    # except Exception as e:
+    #     logger.error(f"Error in fund command for user {user.id}: {str(e)}")
+    #     await update.message.reply_text(
+    #         "⚠️ An error occurred while processing your request. Please try again later."
+    #     )
+    #     raise
 
 
 
